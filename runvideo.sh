54ef5bf6d0afa51a5a8655cbc2b8359c664e3e26
yafangshih <avanshih@gmail.com>
2017-01-03 02:50:40 +0800

    script for running video

---
+++ runvideo.sh
@@ -0,0 +1,8 @@
+max=721
+for i in `seq 1 $max`
+do
+	echo $i
+	var=$(printf './data/input/social/social%04d.jpg' "$i")
+	./bin/EdgePreservedBlur $var
+	matlab -nodisplay -nodesktop -r "LeastSquare"
+done
