103b696c6176cfecb9059801117f7c1386f723bc
yafangshih <avanshih@gmail.com>
2017-01-05 15:02:50 +0800

    +ksize argv

--- runvideo.sh
+++ runvideo.sh
@@ -2,7 +2,7 @@ max=721
 for i in `seq 1 $max`
 do
 	echo $i
-	var=$(printf './data/input/social/social%04d.jpg' "$i")
+	var=$(printf './data/input/social/social%04d.jpg 5' "$i")
 	./bin/EdgePreservedBlur $var
 	matlab -nodisplay -nodesktop -r "xSolver"
 done
